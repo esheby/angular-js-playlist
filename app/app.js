d6cea4605b70334621d705b28885f924c0ba8fa0
esheby <esheby@gmail.com>
2018-02-02 21:55:08 +0700

    Finishes lesson 15

--- app/app.js
+++ app/app.js
@@ -2,26 +2,56 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
+    $scope.removeNinja = function(ninja) {
+
+        var removedNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removedNinja, 1);
+        
+    };
+
+    $scope.addNinja = function() {
+
+        $scope.ninjas.push({
+            name: $scope.newninja.name,
+            belt: $scope.newninja.belt,
+            rate: parseInt($scope.newninja.rate),
+            available: true
+        });
+
+        $scope.newninja.name = "";
+        $scope.newninja.belt = "";
+        $scope.newninja.rate = "";
+        
+    };
+    
     $scope.ninjas = [
         {
             name: "Yoshi",
             belt: "Green",
-            rate: 50
+            rate: 50,
+            available: true,
+            thumb: "http://placehold.it/50x50/666666/ffffff"
         },
         {
             name: "Ryu",
             belt: "yellow",
-            rate: 30
+            rate: 30,
+            available: true,
+            thumb: "http://placehold.it/50x50/666666/ffffff"
         },
         {
             name: "Narto",
             belt: "orange",
-            rate: 10
+            rate: 10,
+            available: false,
+            thumb: "http://placehold.it/50x50/666666/ffffff"
         },
         {
             name: "Saskeh",
             belt: "black",
-            rate: 100
+            rate: 100,
+            available: true,
+            thumb: "http://placehold.it/50x50/666666/ffffff"
         }
     ];
 
