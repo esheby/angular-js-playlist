5fa013e4af9880107b242d9870472aec848b5f24
esheby <esheby@gmail.com>
2018-02-03 17:57:01 +0700

    Finishes lesson 24

--- app/app.js
+++ app/app.js
@@ -8,7 +8,12 @@ myNinjaApp.config(['$routeProvider', function($routeProvider) {
             controller: 'NinjaController'
         })
         .when('/contact', {
-            templateUrl: 'views/contact.html'
+            templateUrl: 'views/contact.html',
+            controller: 'ContactController'
+        })
+        .when('/contact-success', {
+            templateUrl: 'views/contact-success.html',
+            controller: 'ContactController'
         })
         .when('/directory',{
             templateUrl: 'views/directory.html',
@@ -73,3 +78,11 @@ myNinjaApp.controller('NinjaController', ['$scope','$http', function($scope, $ht
     console.log(angular.toJson($scope.ninjas));
 
 }]);
+
+myNinjaApp.controller('ContactController', ['$scope', '$location', function($scope, $location) {
+    
+    $scope.sendMessage = function() {
+        $location.path('/contact-success');
+    }
+
+}])
